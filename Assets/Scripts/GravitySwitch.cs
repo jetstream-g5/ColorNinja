e651cd450fb8be5a235628af3296a9ffc17ae645
Jeffreeyy <17844@ma-web.nl>
2016-01-20 17:15:29 +0100

    Changed colors, added gravity and fixed some bugs

---
+++ Assets/Scripts/GravitySwitch.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GravitySwitch : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    private int Gravity = 10;
+    private bool isUp;
+    private bool inAir = false;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        isUp = false;
+    }
+    
+    void Update()
+    {
+        rb.velocity = new Vector3(0, GravityStatus(), 0);
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            isUp = !isUp;
+        }
+
+    }
+    
+
+    int GravityStatus() 
+    {
+        if (isUp)
+        {
+            Gravity = 10;
+        }
+        else 
+        {
+            Gravity = -10;
+        }
+        return Gravity;
+    }
+
+}
+
+
--- Assets/Scripts/PlayerColor.cs
+++ Assets/Scripts/PlayerColor.cs
@@ -3,25 +3,25 @@ using System.Collections;
 
 public class PlayerColor : MonoBehaviour {
 
-
-    private Color _red = Color.red;
-    private Color _green = Color.green;
+    [SerializeField]
+    private Color _orange;
+    [SerializeField]
+    private Color _blue;
 
     private bool _toggle = true;
 
     private Renderer _renderer;
 
-	// Use this for initialization
+
 	void Start ()
     {
         _renderer = GetComponent<Renderer>();
         ToggleColor();
 	}
 	
-	// Update is called once per frame
 	void Update () 
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKeyDown(KeyCode.LeftControl))
         {
             ToggleColor();
         }
@@ -33,11 +33,11 @@ public class PlayerColor : MonoBehaviour {
 
         if (_toggle)
         {
-            _renderer.material.color = _red;
+            _renderer.material.color = _orange;
         }
         else
         {
-            _renderer.material.color = _green;
+            _renderer.material.color = _blue;
         }
     }
 }
